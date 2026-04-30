336387ad18a6f089151c4120d5aec854bfa70e82
AkshayR-ak <akshayr5665@gmail.com>
2026-04-30 15:58:17 +0530

    Pytest commit

---
+++ unittest/pytest_training/test/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.fixture(scope='function', autouse=True)
+#setup - like a constructor always run first
+def setup_teardown():
+    print('Starting.........')
+    yield#Split into setup & teardown
+    return
+    print('Ending...........')
